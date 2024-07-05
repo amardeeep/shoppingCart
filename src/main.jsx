eb2e55d87a85bf82b66ede1db97c58cb28116907
Deetsy <amardeepdeepak2016@gmail.com>
2024-07-05 22:20:51 +0530

    create footer component having link to github profile

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import Footer from './footer'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <h1>Hello</h1>
+    <Footer/>
   </React.StrictMode>,
 )
