22b38618deaea26183e091f0d8d53116ca526419
Deetsy <amardeepdeepak2016@gmail.com>
2024-07-05 22:42:07 +0530

    create header add name and cart button

---
+++ src/header.jsx
@@ -0,0 +1,10 @@
+
+const Header=()=>{
+    return(
+        <div className="header">
+            <h1>Name of Website</h1>
+            <button>Cart</button>
+        </div>        
+    )
+}
+export default Header
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import Footer from './footer'
+import Header from './header'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Header/>
     <Footer/>
   </React.StrictMode>,
 )
